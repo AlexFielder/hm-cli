c9faa885a0f160f62dfdae800da6509caabcf836
Alex Fielder <Alex.Fielder@graitec.co.uk>
2017-06-05 16:26:01 +0100

    Initial commit with minor changes to "forked" code

---
+++ index.js
@@ -0,0 +1,26 @@
+#!usr/bin/env node
+'use strict';
+
+const program = require('commander'),
+    snapperCommand = require('./snapper/index.js'),
+    pkg = require('./package.json');
+
+program
+    .version(pkg.version)
+    .command('snapper <name>')
+    .description('Scaffolds a new HM snapper with all it\'s dependencie')
+    .option('-p, --package [package]','Package/namespace')
+    .option('-l, --location [location]', 'Location where files will be generated')
+    .action((name, options) => {
+        let loc = options.location || "./" + name;
+        snapperCommand.scaffold(name, options.package, loc, (err, result) => {
+            if(err) {
+                console.log(`Error while scaffolding ${name}Snapper`);
+                console.log(err);
+                return;
+            }
+            console.log(`${name}Snapper generated`);
+        });
+    });
+
+program.parse(prosess.argv);
---
+++ snapper/index.js
@@ -0,0 +1,99 @@
+module.exports = new (function () {
+    const path = require('path'),
+        fs = require('fs'),
+        snapperTemplate = path.join(__dirname, 'Snapper.txt'),
+        productTemplate = path.join(__dirname, 'Product.txt'),
+        resourceTemplate = path.join(__dirname, 'Resource.txt'),
+        configuratorTemplate = path.join(__dirname, './Configurator.txt'),
+        packageTemplate = path.join(__dirname, './Package.txt');
+
+    this.scaffold = (name, package, location, callback) => {
+        location = location || path.dirname(require.main.filename);
+        package = package || extractPackage(location);
+
+        createOrReplaceDirectory(location, err => {
+            fs.readFile(snapperTemplate, 'utf-8', (err, snapperContent) => {
+                if (err) return callback(err);
+                fs.readFile(productTemplate, 'utf-8', (err, productContent) => {
+                    if (err) return callback(err);
+                    fs.readFile(configuratorTemplate, 'utf-8', (err, configuratorContent) => {
+                        if (err) return callback(err);
+                        fs.readFile(packageTemplate, 'utf-8', (err, packageContent) => {
+                            if (err) return callback(err);
+
+                            snapperContent = snapperContent.replace(/##name##/g, name).replace(/##package##/g, package);
+                            productContent = productContent.replace(/##name##/g, name).replace(/##package##/g, package);
+                            resourceContent = resourceContent.replace(/##name##/g, name).replace(/##package##/g, package);
+                            configuratorContent = configuratorContent.replace(/##name##/g, name).replace(/##package##/g, package);
+                            packageContent = packageContent.replace(/##name##/g, name).replace(/##package##/g, package);
+
+                            let snapperFile = `${name}Snapper.cm`,
+                                productFile = `${name}Product.cm`,
+                                geometryFile = `${name}.rs`,
+                                configuratorFile = `${name}Configurator.cm`,
+                                packageFile = `package.cm`;
+
+                            saveFile(path.join(location, snapperFile), snapperContent, (err) => {
+                                if (err) return callback(err);
+                                saveFile(path.join(location, productFile), productContent, (err) => {
+                                    if (err) return callback(err);
+                                    saveFile(path.join(location, geometryFile), resourceContent, (err) => {
+                                        if (err) return callback(err);
+                                        saveFile(path.join(location, configuratorFile), configuratorContent, (err) => {
+                                            if (err) return callback(err);
+                                            saveFile(path.join(location, packageFile), packageContent, (err) => {
+                                                if (err) return callback(err);
+                                                callback(null);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    function extractPackage(location) {
+        const fullLoc = path.resolve(location);
+        if(fullLoc.indexOf('custom') === -1) return 'custom';
+
+        let folders = fullLoc.split('custom');
+        var package = 'custom' + folders[1].replace(/\//g, '.').replace(/\\/g, '.'),
+            segments = package.split('.');
+
+        package = package.replace(segments[segments.length - 1], firstToLowerCase(segments[segments.length - 1]));
+
+        return package;
+    }
+
+    function firstToLowerCase(s) {
+        return s[0].toLowerCase() + s.substring(1, s.length);
+    }
+
+    function createOrReplaceDirectory(dirName, callback) {
+        fs.exists(dirName, (err, exists) => {
+            if (err) return callback(err);
+
+            if (exists) {
+                callback(null);
+            } else {
+                fs.mkdir(dirName, (err) => {
+                    if (err) return callback(err);
+
+                    callback(null);
+                });
+            }
+        });
+    }
+
+    function saveFile(file, content, callback) {
+        fs.writeFile(file, content, 'utf-8', (err) => {
+            if (err) return callback(err);
+
+            callback(null);
+        });
+    }
+})()
